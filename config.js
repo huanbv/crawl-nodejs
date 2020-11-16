8eec0e89d52c20bea509b2715af995e74b60daa1
Huân Bùi <mac@Macs-MacBook-Pro.local>
2020-11-16 21:20:35 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyAbZHzaX3Ku_nyaFUjJhDdSRA5apG8VOUw",
+    authDomain: "news-231a6.firebaseapp.com",
+    databaseURL: "https://news-231a6.firebaseio.com",
+    projectId: "news-231a6",
+    storageBucket: "news-231a6.appspot.com",
+    messagingSenderId: "970787641279",
+    appId: "1:970787641279:web:d25a2d48c40f89ea3cb30a"
+  }
+  // Exports
+  module.exports = firebaseConfig;
